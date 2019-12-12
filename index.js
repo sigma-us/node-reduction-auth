5bb3b15e71dfccb99d51f77570af7da8972c62eb
tipsychubbz <kyle.conley@me.com>
2019-12-12 09:45:02 -0800

    base TS react app with react-router-dom

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 })
