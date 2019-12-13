a24119ea781ff61f9a18b073ac9f445e005de757
tipsychubbz <kyle.conley@me.com>
2019-12-13 15:42:05 -0800

    getting familiar with massive

--- index.js
+++ index.js
@@ -2,19 +2,38 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const massive = require('massive');
+const DB_URL = process.env.DB_URL;
+const bcrypt = require('bcrypt');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+app.get('/api/register', (req, res) => {
+  const db = app.get('db');
+  const user = {
+    firstname: 'first',
+    lastname: 'last',
+    username: 'test',
+    email: 'test@gmail.com',
+    password: 'okokokokokokok'
+  }
+  // db.users.insert(user, (err, res) => {
+  //   console.log(err, res);
+  // })
+  db.users.findOne({id: 2}, function(err, dbres) {
+    console.log(err, dbres);
+  })
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-})
+// postgresql connection register with massive js
+massive(DB_URL).then(massiveInstance => {
+  app.set('db', massiveInstance);
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+  })
+});
