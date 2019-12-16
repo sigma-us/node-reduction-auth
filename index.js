79c0d11d21420e7f7f60904c53ee645726d05741
tipsychubbz <kyle.conley@me.com>
2019-12-15 20:19:18 -0800

    cleaning up route

--- index.js
+++ index.js
@@ -13,21 +13,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/api/register', (req, res) => {
-  const db = app.get('db');
-  const user = {
-    firstname: 'first',
-    lastname: 'last',
-    username: 'test',
-    email: 'test@gmail.com',
-    password: 'okokokokokokok'
+app.post('/api/register', async (req, res) => {
+  try {
+    const db = app.get('db');
+    const response = await db.users.insert(req.body.user);
+    
+    console.log(response);
+    res.status(201).json(response);
+  } catch (e) {
+    res.status(400).json(e)
   }
-  console.log(req.body.user);
-  db.users.insert(req.body.user, function(err, res) {
-    console.log(err, res);
-    if (!err) res.status(200).json(res);
-    else res.status(400).json(err);
-  })
   // db.users.findOne({id: 2}, function(err, dbres) {
   //   console.log(err, dbres);
   // })
